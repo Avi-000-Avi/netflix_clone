e635ec4a3ce6b5e97f3088b8470ffa9c3f2db4a9
Avi-000-Avi <atoppo267@gmail.com>
2020-10-14 00:41:38 +0530

    authv1

--- src/app.js
+++ src/app.js
@@ -2,18 +2,23 @@ import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import {Home, Browse, Signin, Signup} from './pages';
 import * as ROUTES from './constants/routes';
+import {IsUserRedirect} from './helpers/routes';
+
 export default function App() {
+  const user = null;
+
   return (
     <Router>
+         <IsUserRedirect user ={user} loggedInPath={ROUTES.BROWSE} path = {ROUTES.SIGN_IN}>
+        <Signin/>
+        </IsUserRedirect>
+        <IsUserRedirect user ={user} loggedInPath={ROUTES.BROWSE} path = {ROUTES.SIGN_UP}>
+        <Signup/>
+        </IsUserRedirect>
       <Route exact path="/browse">
         <Browse/>
       </Route>
-      <Route exact path="/signin">
-        <Signin/>
-      </Route>
-      <Route exact path="/signup">
-        <Signup/>
-      </Route>
+     
       <Route exact path={ROUTES.HOME}>
         <Home/>
       </Route>
