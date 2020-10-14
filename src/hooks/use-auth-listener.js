33c0ff1a02b3ae1b43152e82d0921d06c9495d7f
Avi-000-Avi <atoppo267@gmail.com>
2020-10-15 00:38:17 +0530

    .

--- src/hooks/use-auth-listener.js
+++ src/hooks/use-auth-listener.js
@@ -1,30 +1,25 @@
 import {useState, useEffect,useContext} from 'react';
 import {FirebaseContext} from '../context/firebase';
 
-
-export default function useAuthListener(){
-    //Check if user is in local storage
-    //Which means we have to store the user stateon any particular action whether they login or logout in the local storage
-    // 
-    const [user,setUser] = useState(
-        JSOn.parse(localStorage.getItem('authUser'))
-    );
-
-    const {firebase} = useContext(FirebaseContext);
-
-    useEffect(() => {
-        const listener = firebase.auth().onAuthStateChanged((authUser) => {
-            if(authUser){
-                localStorage.setItem('authUser',JSON.stringify(authUser));
-                setUser(authUser);
+//Check if user is in local storage
+//Which means we have to store the user stateon any particular action whether they login or logout in the local storage
+export default function useAuthListener() {
+        const [user, setUser] = useState(JSON.parse(localStorage.getItem('authUser')));
+        const { firebase } = useContext(FirebaseContext);
+      
+        useEffect(() => {
+          const listener = firebase.auth().onAuthStateChanged((authUser) => {
+            if (authUser) {
+              localStorage.setItem('authUser', JSON.stringify(authUser));
+              setUser(authUser);
             } else {
-                localStorage.removeItem('authUser');
-                setUser(null);
+              localStorage.removeItem('authUser');
+              setUser(null);
             }
-        });
-
-        return () => listener();
-    },[]);
-
-    return {user};
-}
+          });
+      
+          return () => listener();
+        }, []);
+      
+        return { user };
+      }
