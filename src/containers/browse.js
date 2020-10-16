2459758c51cc2a6bdc66e65e2d51937e4c12a264
Avi-000-Avi <atoppo267@gmail.com>
2020-10-16 18:51:37 +0530

    profietobrowse

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,10 +1,10 @@
-import React, {useState,useContext, useEffect} from 'react';
-import { SelectProfileContainer } from './profiles';
-import {FirebaseContext} from '../context/firebase';
-import { Loading } from '../components';
+import React, { useState, useEffect, useContext } from 'react';
 import Fuse from 'fuse.js';
+import { Card, Header, Loading, Player } from '../components';
 import * as ROUTES from '../constants/routes';
 import logo from '../logo.svg';
+import { FirebaseContext } from '../context/firebase';
+import { SelectProfileContainer } from './profiles';
 import { FooterContainer } from './footer';
 
 
@@ -21,11 +21,27 @@ export function BrowseContainer({ slides }) {
     },[profile.displayName]);
 
     return profile.displayName ? (
-        loading ? (
-        <Loading  src = {user.photoURL}/> 
-        ):(
-        <Loading.ReleaseBody/>
-        )
+    <>
+    {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+            
+            <Header.Frame>
+                <Header.Logo to = {ROUTES.HOME} src ={logo} alt ="Netflix"/>
+                <Header.TextLink>Series</Header.TextLink>
+                <Header.TextLink>Films</Header.TextLink>
+            </Header.Frame>
+            
+            <Header src="joker1">
+                <Header.Feature>
+                <Header.FeatureCallOut>
+                    <Header.Text>
+                    Forever alone in a crowd, failed comedian Arthur Fleck seeks connection as he walks the streets of Gotham
+                    City. Arthur wears two masks -- the one he paints for his day job as a clown, and the guise he projects in a
+                    futile attempt to feel like he's part of the world around him.
+                    </Header.Text>
+                </Header.FeatureCallOut>
+                </Header.Feature>
+            </Header>
+        </>
         ) : (
         <SelectProfileContainer user ={user} setProfile = {setProfile}/>
     );
