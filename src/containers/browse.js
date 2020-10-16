7abc44a6529e8d43f18f5a24870a7c1ed7bfba12
Avi-000-Avi <atoppo267@gmail.com>
2020-10-16 20:16:09 +0530

    browseupdates

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -10,7 +10,8 @@ import { FooterContainer } from './footer';
 
 export function BrowseContainer({ slides }) {
     const [profile, setProfile] = useState({});
-    const [loading,setLoading] = useState(true);
+    const [loading,setLoading] = useState(true)
+    const [searchTerm, setSearchTerm] = useState('');
     const {firebase} = useContext(FirebaseContext);
     const user = firebase.auth().currentUser || {};
 
@@ -33,7 +34,8 @@ export function BrowseContainer({ slides }) {
                 <Header.TextLink>Series</Header.TextLink>
                 <Header.TextLink>Films</Header.TextLink>
                 </Header.Group>
-            <Header.Group>
+                <Header.Group>
+            <Header.Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
             <Header.Profile>
               <Header.Picture src={user.photoURL} />
               <Header.Dropdown>
@@ -46,19 +48,19 @@ export function BrowseContainer({ slides }) {
                 </Header.Group>
               </Header.Dropdown>
             </Header.Profile>
-            </Header.Group>
+          </Header.Group>
             </Header.Frame>
                 
-                
-                <Header.Feature>
-                <Header.FeatureCallOut>
-                    <Header.Text>
-                    Forever alone in a crowd, failed comedian Arthur Fleck seeks connection as he walks the streets of Gotham
-                    City. Arthur wears two masks -- the one he paints for his day job as a clown, and the guise he projects in a
-                    futile attempt to feel like he's part of the world around him.
-                    </Header.Text>
-                </Header.FeatureCallOut>
-                </Header.Feature>
+
+        <Header.Feature>
+          <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
+          <Header.Text>
+            Forever alone in a crowd, failed comedian Arthur Fleck seeks connection as he walks the streets of Gotham
+            City. Arthur wears two masks -- the one he paints for his day job as a clown, and the guise he projects in a
+            futile attempt to feel like he's part of the world around him.
+          </Header.Text>
+          <Header.PlayButton>Play</Header.PlayButton>
+        </Header.Feature>
             </Header>
         </>
         ) : (
